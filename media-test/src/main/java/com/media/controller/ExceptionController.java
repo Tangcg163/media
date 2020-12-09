539c0a38248ea83f831d6bd19558eba0eb65001c
Tangcg <Twy7211x>
2020-12-09 17:47:13 +0800

    测试异常返回格式

--- media-test/src/main/java/com/media/controller/ExceptionController.java
+++ media-test/src/main/java/com/media/controller/ExceptionController.java
@@ -2,6 +2,7 @@ package com.media.controller;
 
 import com.media.api.ApiResult;
 import com.media.exception.MyExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,16 @@ public class ExceptionController {
             int a = 1/0;
         } catch (Exception e) {
            throw new MyExceptionHandler("测试异常处理");
+        }
+        return ApiResult.ok();
+    }
+
+    @GetMapping("/testException")
+    public ApiResult testException() throws Exception{
+        try {
+            int a = 1/0;
+        } catch (Exception e) {
+            throw new MyExceptionHandler("测试异常处理返回格式");
         }
         return ApiResult.ok();
     }
